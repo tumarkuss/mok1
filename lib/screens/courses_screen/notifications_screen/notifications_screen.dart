09aaec0cfa393b7c24bfaaa39d3f5341216cfc00
Tumar Kussainov <tumarkuss@gmail.com>
2023-05-10 15:13:53 +0600

    Profile Screen, Notifications Screen

--- lib/screens/courses_screen/notifications_screen/notifications_screen.dart
+++ lib/screens/courses_screen/notifications_screen/notifications_screen.dart
@@ -3,7 +3,7 @@ import 'package:mok1/components/components.dart';
 import '/data/notifications_data.dart';
 
 class NotificationScreen extends StatelessWidget {
-  const NotificationScreen({super.key});
+  const NotificationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,58 +11,84 @@ class NotificationScreen extends StatelessWidget {
     return Scaffold(
       appBar: DefaultAppBar('Уведомления'),
       body: SingleChildScrollView(
-        child: Align(alignment: AlignmentDirectional.topCenter, child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children:[
-            Container(child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: notifications_data.length,
-              itemBuilder: (BuildContext context, int index ) {
-                return NotificationsBlock(width,index )
-          ;}))
-          
-        ])
+        child: Align(
+          alignment: AlignmentDirectional.topCenter,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: notifications_data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return NotificationsBlock(
+                    width: width,
+                    text: notifications_data[index],
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
 
-Container NotificationsBlock(var width, index){
-  var width;
-  String text = '';
-  text = notifications_data[index];
+class NotificationsBlock extends StatelessWidget {
+  final double width;
+  final String text;
 
-  return Container(
-            margin: const EdgeInsets.all(10),
-            height: 60,
-            width: width,
-            decoration:  BoxDecoration(borderRadius: const BorderRadius.all(Radius.circular(20),),
-            border: Border.all(color: Colors.black54)
+  const NotificationsBlock({
+    required this.width,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      height: 60,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.black54),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(width: 15),
+          SizedBox(
+            height: 35,
+            width: 35,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Container(
+                color: Colors.black54,
+                child: const Icon(
+                  Icons.notification_add_outlined,
+                  color: Colors.white,
+                ),
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(width:15),
-                SizedBox(
-                  height: 35,
-                  width: 35,
-                  child: ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(15),
-                            bottomRight: Radius.circular(15),
-                            topLeft: Radius.circular(15),
-                            topRight: Radius.circular(15)),
-                      child: Container(
-                        color: Colors.black54,
-
-                      child: const Icon(Icons.notification_add_outlined,color: Colors.white,)
-                    ),),
-                  ),
-                  //Text Element 2
-                  Expanded(child: Padding(padding: const EdgeInsets.all(15),child: Text(text, style: const TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w500, fontSize: 13 ))))
-                  
-              ],
-            ), );
+          ),
+          //Text Element 2
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                text,
+                style: const TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 13,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:mok1/data/user_data.dart';
 import 'package:mok1/screens/profile_screen/components/avatar.dart';
 
 import '../../components/components.dart';
+import '../../data/data.dart';
 
 class ProfileScreen extends StatelessWidget {
    ProfileScreen({super.key});
@@ -13,7 +14,7 @@ class ProfileScreen extends StatelessWidget {
     int degree = 2;
     bool gender = true;
     int education_type = 3;
-    String login_name = "tumar.kussainov";
+    String login_name = "kassym-zhomart.tokayev";
     String group = "Test-1234";
     String avatar_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Kassym-Jomart_Tokayev_%282020-02-01%29.jpg/412px-Kassym-Jomart_Tokayev_%282020-02-01%29.jpg";
 
@@ -23,6 +24,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       persistentFooterAlignment: AlignmentDirectional.centerStart,
       appBar: DefaultAppBar('Профиль'),
       body: Column(
@@ -30,6 +32,7 @@ class ProfileScreen extends StatelessWidget {
          ProfileImage(),
           const SizedBox(height: 20),
           Text('$login_name', style: const TextStyle(color: Colors.black, fontSize: 20),),
+          informationBlock(),
 
           
 
@@ -54,5 +57,57 @@ class ProfileScreen extends StatelessWidget {
         
 
     );
+  }
+  
+  informationBlock() {
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+      Container(
+        height: 100,
+        width: 100,
+        decoration:  const BoxDecoration(
+              color: Color.fromARGB(255, 221, 221, 221),
+              borderRadius:  BorderRadius.all(
+                Radius.circular(60.0),
+                
+              )
+            ),
+        //child: ,
+        
+      )
+      ,
+      Container(
+          height: 547.0,
+          color: Colors.transparent,
+          padding: const EdgeInsets.only(top: 40),
+           child:Container(
+            decoration:  const BoxDecoration(
+              color: Color.fromARGB(255, 221, 221, 221),
+              borderRadius:  BorderRadius.only(
+                topLeft:  Radius.circular(40.0),
+                topRight:  Radius.circular(40.0),
+              )
+            ),
+         ),
+        ),
+      informationColumn()]);
+                    
+            
+  }
+  
+  informationColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 65, margin: const EdgeInsets.only(top: 15) ,child: Image.asset('assets/images/graduation_cap.png', color: Colors.black,)
+    ,),
+        informationElement(),
+    ]);
+  }
+  
+  informationElement() {
+    ///Information Element <-- to be ListView()
   }
 }
